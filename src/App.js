748feb4ccc19d0b0492a723e778ae99dfee62271
Bryan Pero <bryanpero@gmail.com>
2020-08-09 18:31:29 -0400

    worked on app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="Employee Directory">
       </header>
     </div>
   );
